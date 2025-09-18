0ad7e7938d93fa39eb33738c1154211979592b4b
Mahamudul Hasan Rafi <raficse30@gmail.com>
2025-09-18 18:43:35 +0600

    cloud stream

--- section 2/message/src/main/java/com/eazybank/message/functions/AccountMessageFunctions.java
+++ section 2/message/src/main/java/com/eazybank/message/functions/AccountMessageFunctions.java
@@ -13,14 +13,17 @@ public class AccountMessageFunctions {
 
     public static final Logger logger = LoggerFactory.getLogger(AccountMessageFunctions.class);
 
+
     @Bean
-    public Function<AccountMessageDto, AccountMessageDto> email() {
+    public Function<AccountMessageDto, AccountMessageDto> input() {
         return accountMessageDto -> {
             logger.info("Processing account message for account number: {}", accountMessageDto.accountNumber());
             // Here you can add the logic to send an email using the details from accountMessageDto
             // For example, you might call an EmailService to send the email
 
             // Simulating email sending
+            System.out.println("Account Mobile Number: " + accountMessageDto.mobileNumber());
+            System.out.println("Preparing to send email...");
             System.out.println("Sending email to: " + accountMessageDto.email());
             System.out.println("Email subject: Account Notification");
             System.out.println("Email body: " + accountMessageDto.message());
@@ -30,19 +33,4 @@ public class AccountMessageFunctions {
         };
     }
 
-    @Bean
-    public Function<AccountMessageDto, Long> sms() {
-        return accountMessageDto -> {
-            logger.info("Processing SMS for account number: {}", accountMessageDto.accountNumber());
-            // Here you can add the logic to send an SMS using the details from accountMessageDto
-            // For example, you might call an SmsService to send the SMS
-
-            // Simulating SMS sending
-            System.out.println("Sending SMS to: " + accountMessageDto.name());
-            System.out.println("SMS body: " + accountMessageDto.message());
-
-            logger.info("SMS sent successfully to: {}", accountMessageDto.name());
-            return accountMessageDto.accountNumber();
-        };
-    }
 }
